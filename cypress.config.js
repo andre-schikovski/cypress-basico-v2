95c106f335133c9a50768b28450d594fbefe7233
Andre Schikovski <andreluiz.schikovski@gmail.com>
2023-12-22 11:06:44 -0300

    Teste 3

--- cypress.config.js
+++ cypress.config.js
@@ -1,6 +1,6 @@
-import { defineConfig } from 'cypress'
+const { defineConfig } = require('cypress')
 
-export default defineConfig({
+module.exports = defineConfig({
   viewportHeight: 880,
   viewportWidth: 1280,
   e2e: {
